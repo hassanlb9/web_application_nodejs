0cd94205d09a7e736b5c3f38b0cf9c7dbd2ef3aa
BACHACHAHassan <93646318+hassanlb9@users.noreply.github.com>
2022-02-13 00:03:25 +0100

    Add files via upload

---
+++ backend/index.js
@@ -0,0 +1,33 @@
+const session = require('express-session');
+const express = require('express');
+const hostname = 'localhost';
+const port = 3001;
+const app = express();
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/json');
+// });
+
+
+app.use(
+    session({
+        secret: 'secret string',
+        resave: false,
+        saveUninitialized: false,
+        cookie: {}
+    })
+);
+
+
+app.listen(port, () => {
+  console.log('Example app listening on port ${port}')
+})
+
+app.get('/', function(req, res){
+  if(!req.session.pageCountByCurrentUserOrAnyNameYouWant)
+      req.session.pageCountByCurrentUserOrAnyNameYouWant = 0;
+  req.session.pageCountByCurrentUserOrAnyNameYouWant++;
+  res.send({
+      pageCount: req.session.pageCountByCurrentUserOrAnyNameYouWant
+  });
+});
